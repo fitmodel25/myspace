c6920c8309b44e146da0804fe84cea3ae2555ac6
Tom Morris <tom@tommorris.org>
2007-11-10 15:29:35 +0000

    added load, name, friends and units

--- myspace.rb
+++ myspace.rb
@@ -3,6 +3,19 @@ require 'hpricot'
 require 'open-uri'
 
 class MySpace
+  attr_accessor :profile, :model
   def initialize
+  end
+
+  def load
+    @model = Hpricot(open("http://www.myspace.com/#{@profile}"))
+  end
+
+  def name
+    (@model/".nametext").first.innerHTML
+  end
+
+  def friends
+    (@model/"span.redbtext").first.innerHTML
   end
 end
--- tests/myspaceTest.rb
+++ tests/myspaceTest.rb
@@ -6,4 +6,31 @@ class MySpaceTest < Test::Unit::TestCase
     my = MySpace.new
     assert_not_nil(my)
   end
+  
+  def test_hpricot_present
+    my = MySpace.new
+    assert_not_nil(Hpricot)
+  end
+  
+  def test_profile_load
+    my = MySpace.new
+    my.profile = "tommorris"
+    assert_equal("tommorris", my.profile)
+    my.load
+    assert_not_nil(my.model)
+  end
+  
+  def test_profile_name
+    my = MySpace.new
+    my.profile = "tommorris"
+    my.load
+    assert_equal("Tom", my.name)
+  end
+  
+  def test_profile_friends
+    my = MySpace.new
+    my.profile = "tommorris"
+    my.load
+    assert_equal("17", my.friends)
+  end
 end
