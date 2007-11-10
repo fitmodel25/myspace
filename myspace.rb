d11f74278ec1a6f9991b6a2a7cb645306d6abcde
Tom Morris <tom@tommorris.org>
2007-11-10 16:43:53 +0000

    added lots of features, fixed a few problems and added dan as a unit test

--- myspace.rb
+++ myspace.rb
@@ -3,19 +3,82 @@ require 'hpricot'
 require 'open-uri'
 
 class MySpace
-  attr_accessor :profile, :model
+  attr_accessor :profile, :hmodel
   def initialize
   end
 
   def load
-    @model = Hpricot(open("http://www.myspace.com/#{@profile}"))
+    @hmodel = Hpricot(open("http://www.myspace.com/#{@profile}"))
   end
 
   def name
-    (@model/".nametext").first.innerHTML
+    (@hmodel/".nametext").first.innerHTML
   end
 
+  def url
+    "http://www.myspace.com/#{@profile}"
+  end
+
+  def sex
+    if (@hmodel/"//td[@class = 'text'][@width = '193']").first.innerHTML.match("Female").to_a.length != 0
+      return 'Female'
+    else
+      return 'Male'
+    end
+  end
+  
+  def age
+    (@hmodel/"//td[@class = 'text'][@width = '193']").first.innerHTML.split[7]
+  end
+  
+  def location
+    (@hmodel/"//td[@class = 'text'][@width = '193']").first.innerHTML.split("<br />")[5].strip
+  end
+  
+  def lastlogin
+    date = (@hmodel/"//td[@class = 'text'][@width = '193']").first.innerHTML.split("<br />")[9].strip[-10..-1]
+    return date[-4..-1] + "-" + date[-7..-6] + "-" + date[-10..-9]
+  end
+  
   def friends
-    (@model/"span.redbtext").first.innerHTML
+    (@hmodel/"span.redbtext").first.innerHTML
+  end
+  
+  def status
+    (@hmodel/"//td[@id = 'ProfileStatus:']").first.innerHTML
+  end
+  
+  def religion
+    (@hmodel/"//td[@id = 'ProfileReligion:']").first.innerHTML
+  end
+  
+  def starsign
+    (@hmodel/"//td[@id = 'ProfileZodiac Sign:']/a").first.innerHTML
+  end
+  
+  def children
+    (@hmodel/"//td[@id = 'ProfileChildren:']").first.innerHTML
+  end
+
+  def education
+    (@hmodel/"//td[@id = 'ProfileEducation:]").first.innerHTML
+  end
+  
+  def smoke
+    case (@hmodel/"//td[@id = 'ProfileSmoke / Drink:']").first.innerHTML.split[0]
+    when "Yes"
+      return true
+    when "No"
+      return false
+    end
+  end
+  
+  def drink
+    case (@hmodel/"//td[@id = 'ProfileSmoke / Drink:']").first.innerHTML.split[2]
+    when "Yes"
+      return true
+    when "No"
+      return false
+    end
   end
 end
--- tests/myspaceTest.rb
+++ tests/myspaceTest.rb
@@ -15,22 +15,50 @@ class MySpaceTest < Test::Unit::TestCase
   def test_profile_load
     my = MySpace.new
     my.profile = "tommorris"
-    assert_equal("tommorris", my.profile)
     my.load
-    assert_not_nil(my.model)
+    assert_not_nil(my.profile)
+    assert_equal("tommorris", my.profile)
+    assert_not_nil(my.hmodel)
   end
   
-  def test_profile_name
+  def test_profile_tommorris
     my = MySpace.new
     my.profile = "tommorris"
     my.load
     assert_equal("Tom", my.name)
+    assert_equal("http://www.myspace.com/tommorris", my.url)
+    assert_equal("17", my.friends)
+    assert_equal("Male", my.sex)
+    assert_equal("22", my.age)
+    assert_equal("United Kingdom", my.location)
+    assert_equal("2007-09-13", my.lastlogin)
+    assert_equal("Single", my.status)
+    assert_equal("Atheist", my.religion)
+    assert_equal("Aquarius", my.starsign)
+    assert_equal("I don't want kids", my.children)
+    assert_equal("In college", my.education)
+    assert_equal(false, my.smoke)
+    assert_equal(false, my.drink)
   end
   
-  def test_profile_friends
+  def test_profile_danbowring
     my = MySpace.new
-    my.profile = "tommorris"
+    my.profile = "danbowring"
     my.load
-    assert_equal("17", my.friends)
+    assert_equal("Dan", my.name)
+    assert_equal("http://www.myspace.com/danbowring", my.url)
+    assert_equal("23", my.friends)
+    assert_equal("Male", my.sex)
+    assert_equal("23", my.age)
+#    assert_equal("United Kingdom", my.location)
+    assert_equal("2007-10-17", my.lastlogin)
+    assert_equal("Single", my.status)
+#    assert_equal("Atheist", my.religion)
+    assert_equal("Taurus", my.starsign)
+    assert_equal("Someday", my.children)
+#    assert_equal("In college", my.education)
+    assert_equal(false, my.smoke)
+    assert_equal(true, my.drink)
   end
+  
 end
